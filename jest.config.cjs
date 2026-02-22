fc88abcfd1bb014f263daf017fc56e745224aefa
Yogesh Aggarwal <yogeshdevaggarwal@gmail.com>
2026-02-22 23:19:41 +0530

    perf: Remove `--runInBand` from test scripts to enable parallel execution and add `isolatedModules` to Jest's TypeScript configuration.

--- jest.config.cjs
+++ jest.config.cjs
@@ -15,6 +15,7 @@ module.exports = {
             tsconfig: {
                allowJs: true,
                module: "esnext",
+               isolatedModules: true,
             },
          },
       ],
